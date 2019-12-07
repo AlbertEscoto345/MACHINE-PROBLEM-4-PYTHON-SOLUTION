0b9e508ec2b9d349b7e37d2e9a4b442ec8c20d2c
AlbertEscoto345 <47411299+AlbertEscoto345@users.noreply.github.com>
2019-12-07 19:40:28 +0800

    Add files via upload

--- MACHPROB4.py
+++ MACHPROB4.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 y_initial=float(input("Input projectile's initial height above ground in meters: "))
 if(y_initial==0):
-    print ('Invalid input. Please try again.')
-    
+    raise ValueError('Invalid input. Please try again.')
+
 velocity=float(input("Input projectile's velocity in meters per second (m/s): "))
 firing_angle=float(input("Input projectile's angle at which it was fired with respect to the x-axis in degrees: "))
 radian_angle=radians(firing_angle)
 x_accel=float(input("Input projectile's acceleration in the x-component in meters per second squared (m/s^2): "))
-y_accel=float(input("Input projectile's acceleration in the y-component in m/s^2: "))
+y_accel=float(input("Input projectile's acceleration in the y-component in m/s^2 (Avoid positive values as they may freeze your PC): "))
 
 xAll=[]
 yAll=[]
@@ -33,4 +33,9 @@ while(y_velocity>0):
     if ordinate<=0:
         break
 
+fig= plt.figure(figsize=(10,5))
 plt.plot(xAll, yAll)
+plt.title('Projectile Trajectory')
+plt.ylabel('Y-position')
+plt.xlabel('X-position')
+plt.grid()
